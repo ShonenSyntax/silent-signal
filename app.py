d9bdb1fd40e9a23c994d962d7c408e7e44a73560
Pratyaksh <reach.pratyaksh@gmail.com>
2025-12-13 23:41:31 +0530

    Show feedback and redirect back to home after Submission

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    submitted = request.args.get("submitted")
+    return render_template("index.html", submitted=submitted)
 
 @app.route("/submit", methods=["POST"])
 def submit():
     report_text = request.form.get("report")
     print(report_text)  #temporary, for learning
 
-    return "Report received anonymously"
+    return redirect(url_for("home", submitted="true"))
 
 if __name__ == "__main__":
     app.run(debug=True)
