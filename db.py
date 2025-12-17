521e30154794e3e2f735060fe74d3ad3573eb496
Pratyaksh <reach.pratyaksh@gmail.com>
2025-12-18 01:57:04 +0530

    Refactor Firebase config to use env-based credentials and add gunicorn for deployment

--- db.py
+++ db.py
@@ -1,9 +1,18 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-#Initialize Firebase only once
+firebase_creds_json = os.getenv("FIREBASE_SERVICE_ACCOUNT")
+
+if not firebase_creds_json:
+    raise ValueError("FIREBASE_SERVICE_ACCOUNT env variable not set")
+
+firebase_creds = json.loads(firebase_creds_json)
+
+# initialize firebase only once
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_key.json")
+    cred = credentials.Certificate(firebase_creds)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
